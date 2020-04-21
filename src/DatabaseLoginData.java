24d06cb0b1ecd12789a03304b7c5bec415e307cf
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2020-04-21 11:11:34 +0200

    I have added some code and added the IP adress of my computer top the whole thing about the database.

---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,8 @@
+public class DatabaseLoginData {
+
+        public static final String DBURL ="192.168.0.8" ;
+        public static final String DBname ="soloadventure" ;
+        public static String port ="3306" ;
+        public static String user ="lucas" ;
+        public static String password ="password" ;
+}
--- src/soloadventure.java
+++ src/soloadventure.java
@@ -1,8 +1,42 @@
-public class soloadventure {
+import java.sql.*;
 
-    public static void main(String[] args) {
+        public class soloadventure {
+
+            public static void main(String[] args) {
+                try {
+                    // Set up connection to database
+                    Connection conn = DriverManager.getConnection(
+                            "jdbc:mysql://"+DatabaseLoginData.DBURL + ":" + DatabaseLoginData.port + "/" + DatabaseLoginData.DBname +
+                                    "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                            DatabaseLoginData.user, DatabaseLoginData.password);
+
+                    // Setup statement
+                    Statement stmt = conn.createStatement();
+                    // Create query and execute
+                    String strSelect = "select body from story where id = 1";
+                    System.out.println("The SQL statement is: " + strSelect + "\n");
+
+                    ResultSet rset = stmt.executeQuery(strSelect);
+
+                    // Loop through the result set and print
+
+                    System.out.println("The records selected are:");
+                    int rowCount = 0;
+                    while(rset.next()) {
+                        String title = rset.getString("body");
+                        System.out.println(title);
+                        ++rowCount;
+                    }
+                    System.out.println("Total number of records = " + rowCount);
+
+                    // Close conn and stmt
+                    conn.close();
+                    stmt.close();
+                } catch(SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
 
 
 
-    }
-}
